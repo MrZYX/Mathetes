e19e7c60cfe3be3821b2c6d1bcc6c7b683ab02f8
MrZYX <mrzyx@mrzyx.de>
2011-05-27 22:46:20 +0200

    added diaspora permalink plugin

---
+++ lib/mathetes/plugins/diaspora-permalink.rb
@@ -0,0 +1,26 @@
+require 'open-uri'
+require 'nokogiri'
+
+module Mathetes; module Plugins
+  class DiasporaPermalink
+    TRUNCATE_AFTER = 150
+    def initialize( mathetes )
+      mathetes.hook_privmsg( :regexp => /http.+\/(status_messages|p)\/\d+/ ) do |message|
+        if message.text =~ /(http.+\/status_messages\/\d+)/
+	  link = $1.gsub('status_messages', 'p')
+        elsif message.text =~ /(http.+\/p\/\d+)/
+          link = $1
+        end
+        if link
+         text = Nokogiri::HTML( open(link) ).xpath( "//div[@id='show_text']/p" ).first().inner_text()
+         text.gsub!(/<[^>]*>/, "")
+         text.gsub!(/[\r\n]+/, " ")
+         if text.length > TRUNCATE_AFTER
+           text = text.slice(0..TRUNCATE_AFTER)
+         end
+         message.answer("[Diaspora] #{text}... #{link}")
+       end
+      end
+    end
+  end
+end; end
